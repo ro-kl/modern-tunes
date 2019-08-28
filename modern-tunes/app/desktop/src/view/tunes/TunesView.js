5390b1a4035d8166cd3189dc73f4e719d12d1e9f
Roman Kluth <roman.kluth@outlook.com>
2019-08-28 10:27:40 +0200

    styling

--- modern-tunes/app/desktop/src/view/tunes/TunesView.js
+++ modern-tunes/app/desktop/src/view/tunes/TunesView.js
@@ -1,16 +1,18 @@
 Ext.define('ModernTunes.view.tunes.TunesView', {
     extend: 'Ext.dataview.DataView',
     xtype: 'tunesview',
+    cls: 'tunes-view',
     scrollable: true,
     layout: {
         type: 'box',
         pack: 'space-around',
         wrap: true
     },
+    itemCls: 'video',
     itemTpl: [
         '<figure>',
         '<div class="thumbnail" style="background-image:url(\'{image}\')"></div>',
-        '<figcaption><div class=\'title\'>{title}</div><div class=\'artist\'>{artist}</div></figcaption>',
+        '<figcaption><div class=\'title\'>{title}</div><div class=\'artist\'>{release_date}</div></figcaption>',
         '</figure>'
     ]
 });
