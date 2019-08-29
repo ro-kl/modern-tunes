2130c041f73e9917b8c350b9d4af6f95de719d07
Roman Kluth <roman.kluth@outlook.com>
2019-08-29 10:41:18 +0200

    video component

--- modern-tunes/app/desktop/src/view/main/MainViewController.js
+++ modern-tunes/app/desktop/src/view/main/MainViewController.js
@@ -8,16 +8,41 @@ Ext.define('ModernTunes.view.main.MainViewController', {
     this.lookupReference('df').setValue(Date.now())
   },
   onThumbSelect: (thumbs, record) => {
-    this.onShowPreview(record.data.artist);
+    this.onShowPreview(record);
   },
   onGridSelect: (grid, records) => {
-    this.onShowPreview(records[0].data.artist);
+    this.onShowPreview(records[0]);
   }
 })
 
 function onShowPreview(record) {
+  let videoConfig = {
+    xtype: 'video',
+    url: record.data.preview,
+    posterUrl: record.data.image
+  };
+  let linkConfig = {
+    docked: 'bottom',
+    xtype: 'component',
+    data: {
+      itunesstore: record.data.itunesstore
+    }
+  };
+  let containerConfig = {
+    xtype: 'container',
+    title: record.data.title + ' — ' + record.data.artist,
+    style: '{background-color: black;}',
+    layout: 'fit',
+    items: [
+      videoConfig,
+      linkConfig
+    ]
+  };
   let vid = Ext.create({
-    xtype: 'preview'
-  })
+    xtype: 'preview',
+    title: record.data.title,
+    layout: 'fit',
+    items: [containerConfig],
+  });
   vid.show();
 }
